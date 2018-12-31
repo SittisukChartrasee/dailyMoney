8ba76381a408b0dd72942f8e6b876493d46653ac
Sittisuk <sittisuk@codefin.io>
2018-12-31 11:54:15 +0700

    first commit

---
+++ screens/MoneyScreen.js
@@ -0,0 +1,10 @@
+import Raect from 'react'
+import { View } from 'react-native'
+
+export default class extends React.Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: 'red' }} />
+    )
+  }
+}
